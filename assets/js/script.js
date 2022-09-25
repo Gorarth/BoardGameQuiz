b5d667b8233ec072b646a255f4008b3fbe434532
Gorarth <Chris-english@hotmail.co.uk>
2022-09-25 19:41:40 +0000

    added basic JS structure

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+const startButton = document.getElementById('start-button')
+const nextButton = document.getElementById('next-button')
+const questionArea = document.getElementById('question-area')
+const question = document.getElementById('question')
+const answerButtons = document.getElementById('answer-buttons')
+
+let shuffledQuestions
+let currentQuestions
+
+function startGame () {
+
+}
+
+function setNextQuestion () {
+
+}
+
+function showQuestion () {
+
+}
+
+function reset () {
+
+}
+
+function selectAnswer(e) {
+
+}
+
+const questions = [
+    {
+        question: '123123',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: false},
+        ]
+    },
+    {
+        question: '123123',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: false},
+        ]
+    },
+    {
+        question: '123123',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: false},
+        ]
+    },
+    {
+        question: '123123',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: false},
+        ]
+    },
+    {
+        question: '123123',
+        answers: [
+            {text: '2', correct: true},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: false},
+        ]
+    },
+]
